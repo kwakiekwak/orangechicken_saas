1e5efe083c165198f3f65235565a9efe3e571155
kwakiekwak <johnkwak1991@yahoo.com>
2015-09-02 20:05:14 +0000

    Added users show action

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+# this is like a spreadsheet
 ActiveRecord::Schema.define(version: 20150902000808) do
 
   create_table "contacts", force: true do |t|
