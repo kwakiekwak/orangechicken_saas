e24dff7be43c6089e155882aacaacc3c00981ae8
kwakiekwak <johnkwak1991@yahoo.com>
2015-09-02 00:56:10 +0000

    Added profile new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,10 @@
 class ProfilesController < ApplicationController 
+  def new
+    # form where a user can fill out their own profile.
+    @user = User.find( params[:user_id] ) # reach into db and find the user that is actually logged in/ (tells us who is logged in)
+    @profile = @user.build_profile # .build_profile method is available to us bc we established the model associations btw the users and profiles 
+    # The association is located in config/locales/routes.rb
+  end
+    
     
 end
--- db/migrate/20150902000808_create_profiles.rb
+++ db/migrate/20150902000808_create_profiles.rb
@@ -1,4 +1,4 @@
-class CreateProfiles < ActiveRecord::Migration  # Created by using bundle exec rails generaate migration ""
+class CreateProfiles < ActiveRecord::Migration  # Created by using bundle exec rails generaate migration "" / Stores information or table
   def change
     create_table :profiles do |t|
       t.integer :user_id # physically ties the two db tables together. (Profile/user)
