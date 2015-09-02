63f93c2cfa1e7ed4b234d5c9773cf727e908a21b
kwakiekwak <johnkwak1991@yahoo.com>
2015-09-02 01:55:41 +0000

    Added profile form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,6 +5,21 @@ class ProfilesController < ApplicationController
     @profile = @user.build_profile # .build_profile method is available to us bc we established the model associations btw the users and profiles 
     # The association is located in config/locales/routes.rb
   end
-    
-    
+  
+  def create #building it again so we can actually save what the user updated
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id] )  
+    else
+      render action: :new  
+    end
+  end
+  
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :job_title, :phone_number, :contact_email, :description)
+          
+    end
 end
